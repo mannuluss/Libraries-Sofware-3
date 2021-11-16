6257c95f0363b9c60d045753bc0d55acf5578d68
Gabriel Pedraza <gabo_pedraza@yahoo.com>
2021-11-16 00:51:47 -0500

    Eliminar constructor de Book.java (generado por Lombok)

--- libraries-spring-boot/src/main/java/example/libraries/Book.java
+++ libraries-spring-boot/src/main/java/example/libraries/Book.java
@@ -5,16 +5,7 @@ import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class Book {
-
-    public Book(String titulo, String iSBN, String autor, String reseña, String valor, Integer unidades) {
-        Titulo = titulo;
-        ISBN = iSBN;
-        Autor = autor;
-        Reseña = reseña;
-        Valor = valor;
-        Unidades = unidades;
-    }
+public class Book {    
 
     public String Titulo, ISBN, Autor, Reseña, Valor;
     public Integer Unidades;
