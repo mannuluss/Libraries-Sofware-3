861a1a38cc2b64f648bb0df52f1d877769d81139
mannuluss <pipe_050401@hotmail.com>
2021-11-16 10:41:39 -0500

    update frontend library

--- frontend-library/src/components/ListaLibros.tsx
+++ frontend-library/src/components/ListaLibros.tsx
@@ -2,24 +2,25 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 interface library {
-    Titulo: string, ISBN: string, Autor: string, Reseña: string, Valor: string;
-    Unidades: number;
+    titulo: string, ISBN: string, autor: string, reseña: string, valor: string;
+    unidades: number;
 }
-const InitialStateLibrary = {
-    Titulo: "", ISBN: "", Autor: "", Reseña: "", Valor: "",
-    Unidades: 0
+const InitialStateLibrary:library = {
+    titulo: "", ISBN: "", autor: "", reseña: "", valor: "",
+    unidades: 0
 }
 export default function ListaLibros() {
     const [libros, setlibros] = useState<library[]>([]);
     const [libraryData, setlibraryData] = useState(InitialStateLibrary);
     useEffect(() => {
         axios.get("http://localhost:8080/api/getlibros").then((res) => {
+            console.log(res.data)
             setlibros(res.data);
         })
     }, [])
 
     function EnviarDatos() {
-        axios.post(`http://localhost:8080/api/agregarlibro?titulo=${libraryData.Titulo}&ISBN=${libraryData.ISBN}&autor=${libraryData.Autor}&resena=${libraryData.Reseña}&valor=${libraryData.Valor}&unidades=${libraryData.Unidades}`)
+        axios.post(`http://localhost:8080/api/agregarlibro?titulo=${libraryData.titulo}&ISBN=${libraryData.ISBN}&autor=${libraryData.autor}&resena=${libraryData.reseña}&valor=${libraryData.valor}&unidades=${libraryData.unidades}`)
             .then((res) => {
                 var datoslibros = JSON.parse(JSON.stringify(libros));
                 var index = datoslibros.findIndex((e: any) => e.ISBN == libraryData.ISBN);
@@ -53,23 +54,23 @@ export default function ListaLibros() {
                 return (<div className="card-container" key={index}>
                     <div className="card-title">
                         <div>
-                            <h3>{libro.Titulo}</h3>
+                            <h3>{libro.titulo}</h3>
                             <div className="name-container">
-                                <p>{libro.Autor}</p>
+                                <p>{libro.autor}</p>
                                 <p>ISBN:{libro.ISBN}</p>
                             </div>
                         </div>
                         <div className="valor">
-                            Precio: {libro.Valor}
+                            Precio: {libro.valor}
                         </div>
                         <div>
-                            Cantidad: {libro.Unidades}
+                            Cantidad: {libro.unidades}
                         </div>
                         <button className="eliminar" onClick={() => deleteitem(libro)}>X</button>
                     </div>
                     <div className="card-content">
                         <p>Reseña</p>
-                        {libro.Reseña}
+                        {libro.reseña}
                         <button className="editar" onClick={() => { editar(libro) }}>Editar</button>
                     </div>
                 </div>);
@@ -83,28 +84,28 @@ export default function ListaLibros() {
             </div>
             <div>
                 <label >titulo</label>
-                <input type="text" value={libraryData.Titulo}
+                <input type="text" value={libraryData.titulo}
                     onChange={(evt) => { ChangesValues("Titulo", evt.target.value) }} />
             </div>
             <div>
                 <label >Autor</label>
-                <input type="text" value={libraryData.Autor}
+                <input type="text" value={libraryData.autor}
                     onChange={(evt) => { ChangesValues("Autor", evt.target.value) }} />
             </div>
             <div>
                 <label >Reseña</label>
-                <input type="text" value={libraryData.Reseña}
+                <input type="text" value={libraryData.reseña}
                     onChange={(evt) => { ChangesValues("Reseña", evt.target.value) }} />
 
             </div>
             <div>
                 <label >Valor</label>
-                <input type="number" value={libraryData.Valor}
+                <input type="number" value={libraryData.valor}
                     onChange={(evt) => { ChangesValues("Valor", evt.target.value) }} />
             </div>
             <div>
                 <label >Unidades</label>
-                <input type="number" value={libraryData.Unidades}
+                <input type="number" value={libraryData.unidades}
                     onChange={(evt) => { ChangesValues("Unidades", evt.target.value) }} />
             </div>
             <button onClick={() => EnviarDatos()}>Enviar</button>
